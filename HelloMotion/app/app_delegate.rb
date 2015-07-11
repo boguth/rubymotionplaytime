98d6f75f401a7553ea7f098f79b8e15ea300179a
boguth <bo.guthrie.mail@gmail.com>
2015-07-11 16:12:42 -0500

    Comitting more comments in app_delegate

--- HelloMotion/app/app_delegate.rb
+++ HelloMotion/app/app_delegate.rb
@@ -3,6 +3,7 @@
 # In iOS-land, when the user launches our app the system sets up a bunch of stuff for us. We need to give the OS an object which can respond to different events during that process; we refer to that object as the "application delegate". It gets callbacks for when the app starts, ends, goes to the background, gets a push notification, all sorts of fun stuff. In other words, this is RubyMotion's API for the Apple iOS.
 
 class AppDelegate
+    # The arguments in this method definition are wacky. This is an exmaple of an Objectice-C convention. See README for an explanation of named arguments.
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     rootViewController = UIViewController.alloc.init
     rootViewController.title = 'HelloMotion'
@@ -17,4 +18,5 @@ class AppDelegate
     true
   end
 end
+
 
